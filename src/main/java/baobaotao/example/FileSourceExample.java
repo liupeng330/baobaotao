1485e1047648d48048c8c2a48f0829c8591543c6
liupeng330 <330liupeng@gmail.com>
2016-02-23 00:28:30 +0800

    adding test file

---
+++ src/main/java/baobaotao/example/FileSourceExample.java
@@ -0,0 +1,34 @@
+package baobaotao.example;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FileSourceExample
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            String filePath = "/Users/peng/IdeaProjects/baobaotao/target/classes/test/myconf.txt";
+
+            // 使用系统文件路径方式加载文件
+            Resource res1 = new FileSystemResource(filePath);
+
+            // 使用类路径方式加载文件
+            Resource res2 = new ClassPathResource("test/myconf.txt");
+
+            InputStream ins1 = res1.getInputStream();
+            InputStream ins2 = res2.getInputStream();
+            System.out.println("res1:" + res1.getFilename());
+            System.out.println("res2:" + res2.getFilename());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
