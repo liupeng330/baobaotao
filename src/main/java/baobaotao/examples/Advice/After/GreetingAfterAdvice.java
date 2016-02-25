b794c540468b98a48a84eb0a639a45f63e487f69
liupeng330 <330liupeng@gmail.com>
2016-02-25 19:18:53 +0800

    adding mvc support

---
+++ src/main/java/baobaotao/examples/Advice/After/GreetingAfterAdvice.java
@@ -0,0 +1,14 @@
+package baobaotao.examples.Advice.After;
+
+import org.springframework.aop.AfterReturningAdvice;
+
+import java.lang.reflect.Method;
+
+public class GreetingAfterAdvice implements AfterReturningAdvice
+{
+    @Override
+    public void afterReturning(Object o, Method method, Object[] objects, Object o1) throws Throwable
+    {
+        System.out.println("Please enjoy yourself !!");
+    }
+}
---
+++ src/main/java/baobaotao/examples/Advice/TestAdviceWithSpringConfig.java
@@ -0,0 +1,17 @@
+package baobaotao.examples.Advice;
+
+import baobaotao.examples.Advice.Waiter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//使用spring配置文件完成对方法的前置增强
+public class TestAdviceWithSpringConfig
+{
+    public static void main(String[] args)
+    {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("aop/advice-config.xml");
+        Waiter waiter = (Waiter) ctx.getBean("waiter");
+        waiter.greetTo("John");
+        waiter.serveTo("Tom");
+    }
+}
