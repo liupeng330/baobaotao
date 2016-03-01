ad51ff7e4af636ee4186bc30df565c617c9ddea2
liupeng330 <330liupeng@gmail.com>
2016-03-01 12:07:31 +0800

    iceaadding mvc support

--- src/main/java/baobaotao/examples/Advisor/TestStaticAdvisor.java
+++ src/main/java/baobaotao/examples/Advisor/TestStaticAdvisor.java
@@ -8,7 +8,8 @@ public class TestStaticAdvisor
     public static void main(String[] args)
     {
 //        ApplicationContext ctx = new ClassPathXmlApplicationContext("aop/advisor-static-config.xml");
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("aop/advisor-regexp-config.xml");
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("aop/advisor-regexp-config.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("aop/advisor-auto-proxy-regexp-config.xml");
         Waiter waiter = (Waiter) ctx.getBean("waiter");
         Seller seller = (Seller) ctx.getBean("seller");
         waiter.greetTo("John");
